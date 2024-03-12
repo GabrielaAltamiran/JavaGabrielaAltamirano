4a2ca527ea871c949bad85b461154bd6129300ab
gabielanieto28 <gabrielanieto289@gmail.com>
2024-03-12 12:45:51 -0500

    TextCalculadora y funciones

--- Reto8.1/src/com/krakedev/TextCalculadora.java
+++ Reto8.1/src/com/krakedev/TextCalculadora.java
@@ -6,11 +6,27 @@ public class TextCalculadora {
 		Calculadora calcu;
 		int resultadoSuma;
 		double resultadoResta;
+		double resultadoMultiplicar;
+		double resultadoDividir;
+		double resultadoPromedio;
+		//MostrarResultado
+		Calculadora n;
+		n = new Calculadora();
+		n.mostrarResultado();
+		//
 		calcu = new Calculadora();
 		resultadoResta = calcu.restar(10, 5);
 		resultadoSuma  = calcu.sumar(5,8);
+		resultadoMultiplicar = calcu.multiplicar(3, 5);
+		resultadoDividir = calcu.dividir(81, 9);
+		resultadoPromedio = calcu.promediar(3, 10, 10);
+		
 		System.out.println("Resultado suma : "+resultadoSuma);
 		System.out.println("Resultado resta :"+resultadoResta);
+		System.out.println("Resultado multiplicacion :"+resultadoMultiplicar);
+		System.out.println("Resultado divicion :"+resultadoDividir);
+		System.out.println("Resultado promedio :"+resultadoPromedio);
+		
 		
 		
 
