b38eda6e6aa84b44abd5b3dfe2342a6545eb7b6f
gabielanieto28 <gabrielanieto289@gmail.com>
2024-03-13 09:25:12 -0500

    Ejercio constructores-primera clases

--- Reto8.1/src/com/krakedev/Auto.java
+++ Reto8.1/src/com/krakedev/Auto.java
@@ -7,6 +7,14 @@ public class Auto {
 	public String getMarca() {
 		return marca;
 	}
+	
+	public Auto(String marca, int anio,double precio){
+		this.marca = marca;
+		this.anio = anio;
+		this.precio = precio;
+		
+	}
+	
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
